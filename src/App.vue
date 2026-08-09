dbb449fdfb3a6c382779531d50fe24377d5d2996
eldercs <eldercsdd@gmail.com>
2026-08-09 03:09:06 +0200

    habe css style entstandet

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <p>{{ info }}</p>
+  <h1 class="p-8 text-xl">{{ info }}</h1>
   <h2>Vue test</h2>
-  <input type="text" v-model="userName" @input="insertData($event.target.value)" placeholder="Name">
-  <input type="password" v-model="userPassword" placeholder="Passwort" id="">
-  <input type="email" v-model="userEmail" placeholder="Email">
-
-  <button @click="addUser()">Click</button>
-  <div v-if="user.length <= 0">Es gibt keine Users</div>
-  <div class="bg-sky-50" v-for="(el, index) in user" :key="index">
-    <p>{{ el.name }}</p>
-    <h2>{{ el.email }} {{ el.password }}</h2>
+  <div class ="flex flex-col px-4 max-w-300 mx-auto my-2">
+    <input type="text" class="p-2 mt-2 border rounded-xs" v-model="userName" @input="insertData($event.target.value)" placeholder="Name" id="User" />
+    <input type="password" class="p-2 mt-2 border rounded-xs" v-model="userPassword" placeholder="Passwort" id="passwort" />
+    <input type="email" class="p-2 mt-2 border rounded-xs" v-model="userEmail" placeholder="Email" id="Email" />
   </div>
+
+
+  <button class="bg-red-500 text-white py-4 px-6 border rounded-sm" @click="addUser()">Click</button>
+  <div v-if="users.length <= 0">Es gibt keine Users</div>
+  <UserProfil v-for="(el, index) in users" :key="index" :user="el" @click="deleteUser(index)"></UserProfil>
 </template>
 <script>
 
-
+import UserProfil from './components/UserProfil.vue'
 /* import HelloWorld from './components/HelloWorld.vue' */
 
 export default {
   name: 'App',
   components: {
-    
+    UserProfil
   },
   data(){
     return {
       info: "Title", 
       andereTest: "Text",
-      user: [],
+      users: [],
       userName: "",
       userPassword: "",
       userEmail: ""
@@ -42,12 +42,15 @@ export default {
     },
     addUser(){
       if(this.userName != "" && this.userPassword != "" && this.userEmail != "")
-      this.user.push({
+      this.users.push({
         name: this.userName,
         email: this.userEmail,
         password: this.userPassword
       })
-    }
+    }, 
+    deleteUser(indexUser){
+      this.users.splice(indexUser, 1);
+    },
   }
 }
 </script>
@@ -62,6 +65,6 @@ h2{
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/UserProfil.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class ="flex border px-4 py-2 max-w-300 mx-auto my-2">
+    <div class="text-center">
+      <p>{{ user.name }}</p>
+      <h2>{{ user.email }} {{ user.password }}</h2>
+    </div>
+    <button class="absolute  right-2 bg-red-500 w-40 rounded text-white ml-auto py-3 px-6 border rounded-sm">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    user:{
+      type:Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
